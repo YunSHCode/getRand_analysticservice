abfca6c1db85f468f8d9468939d8c44d5f4a2a8a
CoderJDan <coder.jangdaniel@gmail.com>
2024-12-17 18:36:34 +0900

    first commit_analysticservice

---
+++ src/main/java/com/example/getrand_analysticservice/GetRandAnalysticserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.getrand_analysticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GetRandAnalysticserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetRandAnalysticserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/getrand_analysticservice/GetRandAnalysticserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getrand_analysticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetRandAnalysticserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
