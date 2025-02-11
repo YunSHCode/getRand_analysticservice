44c36f9afc561f419e0d8568532ef70b4b1bfc2b
YunSHCode <tjdgns7369@gmail.com>
2025-02-11 15:28:37 +0900

    ci test

--- src/main/java/com/example/getrand_analysticservice/controller/KeywordController.java
+++ src/main/java/com/example/getrand_analysticservice/controller/KeywordController.java
@@ -16,6 +16,6 @@ public class KeywordController {
         trendService.pastOneYear(query);
         trendService.fetchRelatedQueries(query);
         trendService.fetchRelatedQueries(query);
-        return "OK12";
+        return "OK123";
     }
 }
