6d214c59cf4c67741a7f03ff6d72b088b1bc7a57
YunSHCode <tjdgns7369@gmail.com>
2025-02-11 16:37:54 +0900

    ci test

--- src/main/java/com/example/getrand_analysticservice/client/DataCollectionServiceClient.java
+++ src/main/java/com/example/getrand_analysticservice/client/DataCollectionServiceClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "datacollectionservice")
+@FeignClient(name = "datacollectionservice", url = "http://collection")
 public interface DataCollectionServiceClient {
     @GetMapping("/datacollectionservice/defaultPastOY/findall")
     public List<DefaultPastOYResponseDTO> getAllDefaultPastOY();
